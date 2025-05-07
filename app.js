94ca8a34f599f27d145f840d04f9f0d3bc89edfe
sandeepvaishya326 <sandeepvaishya326@gmail.com>
2025-05-07 13:06:42 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+res.send('Hello from Cloud Run!');
+});
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}`));
